f80357fcedb4475f312a3e43cfc23cf75ce7c3f5
egonlr <egonlr@hotmail.com>
2021-12-27 18:38:10 -0300

    Criando nova classes

--- src/main.kt
+++ src/main.kt
@@ -1,87 +1,28 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaComportamentosConta()
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf ="111.111.111-11",
+        salario = 1000.0)
 
-}
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println("bonificação ${alex.bonificacao()}")
 
-fun testaComportamentosConta() {
-    val contaAlex = Conta(titular = "Alex", numero = 1000)
-    contaAlex.deposita(200.0)
+    val fran = Gerente(nome = "Fran", cpf = "222.111.111-11", salario = 2000.0, senha = 1234)
 
-    val contaFran = Conta(numero = 1001, titular = "Fran")
-    contaFran.deposita(300.0)
+    println("nome ${fran.nome}")
+    println("cpf ${fran.cpf}")
+    println("salario ${fran.salario}")
+    println("bonificação ${fran.bonificacao()}")
 
-    println(contaFran.titular)
-    println(contaFran.numero)
-    println(contaFran.saldo)
-
-    println(contaAlex.titular)
-    println(contaAlex.numero)
-    println(contaAlex.saldo)
-
-    println("depositando na conta do Alex")
-    contaAlex.deposita(50.0)
-    println(contaAlex.saldo)
-
-    println("depositando na conta da Fran")
-    contaFran.deposita(70.0)
-    println(contaFran.saldo)
-
-    println("sacando na conta do Alex")
-    contaAlex.saca(250.0)
-    println(contaAlex.saldo)
-
-    println("sacando na conta da Fran")
-    contaFran.saca(100.0)
-    println(contaFran.saldo)
-
-    println("saque em excesso na conta do Alex")
-    contaAlex.saca(100.0)
-    println(contaAlex.saldo)
-
-    println("saque em excesso na conta da Fran")
-    contaFran.saca(500.0)
-    println(contaFran.saldo)
-
-    println("Transferência da conta da Fran para o Alex")
-
-    if (contaFran.transfere(destino = contaAlex, valor = 300.0)) {
-        println("Transferência sucedida")
+    if (fran.autentica(1234)) {
+        println("Autenticou com sucesso")
     } else {
-        println("Falha na transferência")
+        println("Falha na autenticacao")
     }
-
-    println(contaAlex.saldo)
-    println(contaFran.saldo)
 }
 
-class Conta(
-    var titular: String,
-    val numero: Int
-) {
-    var saldo = 0.0
-        private set
 
-    fun deposita(valor: Double) {
-        if (valor > 0) {
-            this.saldo += valor
-        }
-    }
-
-    fun saca(valor: Double) {
-        if (saldo >= valor) {
-            saldo -= valor
-        }
-    }
-
-    fun transfere(valor: Double, destino: Conta): Boolean {
-        if (saldo >= valor) {
-            saldo -= valor
-            destino.deposita(valor)
-            return true
-        }
-        return false
-    }
-
-}
