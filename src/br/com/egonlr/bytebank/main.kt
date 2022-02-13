9481542700c5885f319b3812cf929ed64319538f
egonlr <egonlr@hotmail.com>
2022-02-13 07:48:33 -0300

    Conhecendo as funções de alta ordem

--- src/br/com/egonlr/bytebank/main.kt
+++ src/br/com/egonlr/bytebank/main.kt
@@ -1,52 +1,30 @@
 package br.com.egonlr.bytebank
 
+import br.com.egonlr.bytebank.modelo.Endereco
+import br.com.egonlr.bytebank.teste.teste
+
 fun main() {
-//    testaTipoFuncaoReferencia()
-//    testaTipoFuncaoClasse()
-    val minhaFuncaoLambda = {a: Int, b: Int ->
-        a + b
+    Endereco().let{
+
     }
-    println(minhaFuncaoLambda(15, 10))
+    "".let(::testeRecebeString)
 
-    val minhaFuncaoAnonima= fun(a: Int, b: Int): Int {
-        return a + b
+    1.let{
+
     }
-    println(minhaFuncaoAnonima(10, 20))
-
-    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
-        if (salario > 1000.0) {
-            return@lambda salario + 50
-        }
-        return@lambda salario + 100.0
-    }
-    println(calculaBonificacao(500.0))
-
-    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double{
-        if (salario > 1000.0) {
-            return salario + 50
-        }
-        return salario + 100.0
-    }
-    println(calculaBonificacaoAnonima(10000.0))
+    teste(1, {})
 }
 
-fun testaTipoFuncaoClasse() {
-   val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
-   println(minhaFuncaoClasse(10, 10))
+fun testeRecebeString(valor: String){
+
+}
+//higher order function (funçao que recebe funçao)
+fun teste(teste: Int, bloco: () -> Unit) {
+
 }
 
-fun testaTipoFuncaoReferencia() {
-    val minhaFuncao: (Int, Int) -> Int = ::soma
-    println(minhaFuncao(10,5))
-}
 
-fun soma(a: Int, b:Int) : Int {
-    return (a + b)
-}
 
-class Soma: (Int, Int) -> Int {
-    override fun invoke(a: Int, b: Int): Int = a + b
-}
 
 
 
---
+++ src/br/com/egonlr/bytebank/teste/testaTipoFuncao.kt
@@ -0,0 +1,50 @@
+package br.com.egonlr.bytebank.teste
+
+public fun testaFuncaoAnonima() {
+    val minhaFuncaoAnonima = fun(a: Int, b: Int): Int {
+        return a + b
+    }
+    println(minhaFuncaoAnonima(10, 20))
+
+
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if (salario > 1000.0) {
+            return salario + 50
+        }
+        return salario + 100.0
+    }
+    println(calculaBonificacaoAnonima(10000.0))
+}
+
+fun testaFuncaoLambda() {
+    val minhaFuncaoLambda = { a: Int, b: Int ->
+        a + b
+    }
+    println(minhaFuncaoLambda(15, 10))
+
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
+        if (salario > 1000.0) {
+            return@lambda salario + 50
+        }
+        return@lambda salario + 100.0
+    }
+    println(calculaBonificacao(500.0))
+}
+
+fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
+    println(minhaFuncaoClasse(10, 10))
+}
+
+fun testaTipoFuncaoReferencia() {
+    val minhaFuncao: (Int, Int) -> Int = ::soma
+    println(minhaFuncao(10,5))
+}
+
+fun soma(a: Int, b:Int) : Int {
+    return (a + b)
+}
+
+class Soma: (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int = a + b
+}
