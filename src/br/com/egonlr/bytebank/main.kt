ac315ff7b5f72a27f6885cccbdc9f5168f6f7859
egonlr <egonlr@hotmail.com>
2022-02-18 09:28:34 -0300

    Testando o run

--- src/br/com/egonlr/bytebank/main.kt
+++ src/br/com/egonlr/bytebank/main.kt
@@ -1,10 +1,32 @@
 package br.com.egonlr.bytebank
 
-import br.com.egonlr.bytebank.modelo.Autenticavel
-import br.com.egonlr.bytebank.modelo.Endereco
-import br.com.egonlr.bytebank.modelo.SistemaInterno
+import br.com.egonlr.bytebank.modelo.*
 
 fun main() {
+    val taxaAnual = 0.05
+    val taxaMensal = taxaAnual / 12
+    println("Taxa mensal $taxaMensal")
+
+    val contaPoupanca = ContaPoupanca(Cliente(nome = "Alex", cpf = "111.111.111-11", senha = 1234), 1000)
+    contaPoupanca.run {
+            deposita(1000.0)
+            saldo * taxaMensal
+        }.let {rendimentoMensal ->
+            println("Rendimento mensal: $rendimentoMensal")
+        }
+
+    val rendimentoAnual = run {
+        var saldo: Double = contaPoupanca.saldo
+        repeat(12) {
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+    println("Rendimento anual: $rendimentoAnual")
+
+}
+
+fun testaWith() {
     with(Endereco()) {
         logradouro = "rua vegueiro"
         numero = 3185
@@ -14,10 +36,9 @@ fun main() {
         cep = "02310-063"
         complemento = "apartamento"
         completo()
-    }.let {endercoCompleto: String ->
+    }.let { endercoCompleto: String ->
         println(endercoCompleto)
     }
-
 }
 
 
